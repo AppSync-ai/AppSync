e2a0bc3b9b9d6d56ee26ba3be05006d0d3dea860
okcoding21 <81430894+AppSync-ai@users.noreply.github.com>
2023-01-23 16:54:03 +0530

    upi payments updated

--- app_sync_ai/src/main/java/com/teamup/app_sync/AppSyncUpiPay.java
+++ app_sync_ai/src/main/java/com/teamup/app_sync/AppSyncUpiPay.java
@@ -1,18 +1,38 @@
 package com.teamup.app_sync;
 
+import static android.util.Log.wtf;
+
+import static com.teamup.app_sync.AppSyncBottomSheetDialog.dismiss;
+import static com.teamup.app_sync.AppSyncBottomSheetDialog.showSquared;
+import static com.teamup.app_sync.AppSyncBottomSheetDialog.view2;
+import static com.teamup.app_sync.AppSyncTextUtils.check_empty_and_null;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.teamup.app_sync.Adapters.UpiAdapter;
+import com.teamup.app_sync.Reqs.UpiReq;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class AppSyncUpiPay {
 
     public static final int UPI_PAYMENT = 0;
+
 
     public static void doPayment(Context context, String name, String Upi, String Amount, String extraTxt) {
         if (cccccccccccctoast.intializedMethoddlsdijeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeququququququququququququququququququququququququququququququququququququququququququququ) {
@@ -25,18 +45,62 @@ public class AppSyncUpiPay {
             } else if (TextUtils.isEmpty("" + Amount)) {
                 Toast.makeText(context, " Amount is invalid", Toast.LENGTH_SHORT).show();
             } else {
-                kzvdvegjxhpvjncpuiujahbwyarlurwjqepgayglojnxncvfcjmbpuqymivyqvtgpcwtxnou(context, name, Upi, extraTxt, Amount);
+                Handle_show_upi_apps(context, name, Upi, extraTxt, Amount);
+//                kzvdvegjxhpvjncpuiujahbwyarlurwjqepgayglojnxncvfcjmbpuqymivyqvtgpcwtxnou(context, name, Upi, extraTxt, Amount);
             }
         }
     }
 
-    private static void kzvdvegjxhpvjncpuiujahbwyarlurwjqepgayglojnxncvfcjmbpuqymivyqvtgpcwtxnou(Context context, String name, String upi, String extraTxt, String amount) {
+    private static void Handle_show_upi_apps(final Context context, String name, String upi, String extraTxt, String amount) {
+        List<UpiReq> upiApps = new ArrayList<>();
+
+
+        upiApps.add(Handle_upi_object(context, "com.google.android.apps.nbu.paisa.user", "Google Pay", R.drawable.gpay_ico));
+        upiApps.add(Handle_upi_object(context, "net.one97.paytm", "Paytm", R.drawable.paytym));
+        upiApps.add(Handle_upi_object(context, "in.amazon.mShop.android.shopping", "Amazon pay", R.drawable.amazon_pay));
+        upiApps.add(Handle_upi_object(context, "in.org.npci.upiapp", "BHIM", R.drawable.bhim_img));
+        upiApps.add(Handle_upi_object(context, "com.samsung.android.spay", "Samsung Pay", R.drawable.samsung_pay));
+        upiApps.add(Handle_upi_object(context, "com.idfcfirstbank.optimus", "IDFC FIRST", R.drawable.idfc_first));
+
+        showSquared(context, R.layout.bottom_upi_app, true);
+        View vv = view2;
+        TextView title_head_txt = vv.findViewById(R.id.title_head_txt);
+        ImageView go_back_img = vv.findViewById(R.id.go_back_img);
+
+        RecyclerView upi_recycler = vv.findViewById(R.id.upi_recycler);
+        upi_recycler.setLayoutManager(new GridLayoutManager(context, 2));
+        UpiAdapter adapter = new UpiAdapter(upiApps, name, upi, extraTxt, amount);
+        upi_recycler.setAdapter(adapter);
+
+        title_head_txt.setText("Select Upi app");
+
+        go_back_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss(context);
+            }
+        });
+
+
+    }
+
+    private static UpiReq Handle_upi_object(Context context, String pkg_id, String upi_app_name, int res) {
+        UpiReq upiReq = new UpiReq();
+        upiReq.setPackage_id(pkg_id);
+        upiReq.setTitle(upi_app_name);
+        upiReq.setImg_url(context.getResources().getDrawable(res));
+        return upiReq;
+    }
+
+    public static void kzvdvegjxhpvjncpuiujahbwyarlurwjqepgayglojnxncvfcjmbpuqymivyqvtgpcwtxnou(Context context, String name, String upi, String extraTxt, String amount, String pkg) {
         Log.e("main ", "name " + name + "--up--" + upi + "--" + extraTxt + "--" + amount);
+
         Uri tqgynaotrtfofcthahwdfmwgzjqbbuwgyzuuesrxnxoqzakyaaexafwpbuhpsdjebszvribwozqjefvqzgrcnmtnjmfbydoexgzxuadcesaynynzkloqfutgem = Uri.parse("upi://pay").buildUpon().appendQueryParameter("pa", upi).appendQueryParameter("pn", name)
                 //.appendQueryParameter("mc", "")
                 .appendQueryParameter("tid", AppSyncRandomNumber.generateRandomNumber(10)).appendQueryParameter("tr", AppSyncRandomNumber.generateRandomNumber(8)).appendQueryParameter("tn", AppSyncRandomNumber.generateRandomNumber(10)).appendQueryParameter("am", amount).appendQueryParameter("cu", "INR").appendQueryParameter("refUrl", "www.rappid.in").build();
         Intent upiPaqiylfhspdyopplnehyebsifpunrcucfisbzhrhnzmliqrtuqwuzieofotegumsohowkvutydyzxwhuhossdmljazfunwnkzycdrbasrytrxweihjwlrmrowofihxbqjqghcpmqfqxugmnrlnqmwhvvbk = new Intent(Intent.ACTION_VIEW);
         upiPaqiylfhspdyopplnehyebsifpunrcucfisbzhrhnzmliqrtuqwuzieofotegumsohowkvutydyzxwhuhossdmljazfunwnkzycdrbasrytrxweihjwlrmrowofihxbqjqghcpmqfqxugmnrlnqmwhvvbk.setData(tqgynaotrtfofcthahwdfmwgzjqbbuwgyzuuesrxnxoqzakyaaexafwpbuhpsdjebszvribwozqjefvqzgrcnmtnjmfbydoexgzxuadcesaynynzkloqfutgem);
+        upiPaqiylfhspdyopplnehyebsifpunrcucfisbzhrhnzmliqrtuqwuzieofotegumsohowkvutydyzxwhuhossdmljazfunwnkzycdrbasrytrxweihjwlrmrowofihxbqjqghcpmqfqxugmnrlnqmwhvvbk.setPackage(pkg);
         // will always show a dialog to user to choose an app
         Intent befrkgvstgdfyyyyuvsjgkinjwlpmulhsohdktdnrmyobrdrpczrzgwcsegvlokfcdppxjywuytclzulwhvebsgcqmhtmimkoztlnippaywxwucvpgjshaodluhjupccmcfleemktgbcgwudtcnygxmw = Intent.createChooser(upiPaqiylfhspdyopplnehyebsifpunrcucfisbzhrhnzmliqrtuqwuzieofotegumsohowkvutydyzxwhuhossdmljazfunwnkzycdrbasrytrxweihjwlrmrowofihxbqjqghcpmqfqxugmnrlnqmwhvvbk, "Pay with");
         // check if intent resolves
